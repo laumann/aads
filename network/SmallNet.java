04fe34baa162795dd6d4c4b57f421807cc56ab47
Philip Pickering <pgpick@gmx.at>
2012-05-11 20:56:34 +0200

    SmallNet: back to original example, some output modifications

--- network/SmallNet.java
+++ network/SmallNet.java
@@ -4,9 +4,9 @@ class SmallNet extends MF {
     public static void main(String[] args) {
 	int [][] cs = new int[5][5];
 	uconnect(0, 1, 5, cs);
-	uconnect(0, 2, 11, cs);
+	uconnect(0, 2, 10, cs);
 	uconnect(1, 3, 7, cs);
-	uconnect(2, 3, 8, cs);
+	uconnect(2, 3, 7, cs);
 	uconnect(2, 4, 3, cs);
 	uconnect(3, 4, 12, cs);
 	
@@ -22,11 +22,13 @@ class SmallNet extends MF {
 	//cost = mkCost(cost, cs);
 
 	//cs = conv2MaxFlow(cs);
-	print(cs);
-	print(cost);
+	// since it already is a max flow
+
+	//	print(cs);
+	//print(cost);
 	int [][] map = createmap(cs);
 	try {
-	    LpSolve solver = MF.conv2LP(cs, 0, 4, map);
+	    LpSolve solver = MF.conv2LP(cs, s, t, map);
 	    solver.solve();
 	    double objective = solver.getObjective();
 	    System.out.println("Value of objective function: " + solver.getObjective());
@@ -34,8 +36,8 @@ class SmallNet extends MF {
 	    for (int i = 0; i < var.length; i++) {
 		System.out.println("Value of var[" + i + "] = " + var[i]);
 	    }
-	    solver.printDuals();
-	    /*
+	    //	    solver.printDuals();
+	    
 	    System.out.println("min cost:");	    
 	    LpSolve mincost = conv2MinCostLP(cs, cost, map, s, t, objective);
 	    mincost.solve();
@@ -45,8 +47,8 @@ class SmallNet extends MF {
 		System.out.println("Value of var[" + i + "] = " + var[i]);
 	    }
 
-	    mincost.printLp();
-	    */
+	    //mincost.printLp();
+	    
 
 	} catch (Exception e) {
 	    e.printStackTrace();
