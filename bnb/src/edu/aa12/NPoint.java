b3db85af33bdb64c9506a4ac327b21324a2b84f0
Philip Pickering <pgpick@gmx.at>
2012-05-24 15:52:11 +0200

    create NPoint to solve compilation errors of supplied classes

---
+++ bnb/src/edu/aa12/NPoint.java
@@ -0,0 +1,15 @@
+package edu.aa12;
+
+import ProGAL.geom2d.Point;
+
+/**
+ * Author: Philip Pickering <pgpick@gmx.at>
+ * Date: 5/24/12
+ * Time: 3:47 PM
+ */
+public class NPoint extends Point {
+    NPoint(double[] coords) {
+        super(coords[0], coords[1]);
+    }
+
+}
