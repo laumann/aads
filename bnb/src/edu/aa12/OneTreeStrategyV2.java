255c7110d8182de0648b3d0f322bda49f6dee8ac
Philip Pickering <pgpick@gmx.at>
2012-05-30 16:31:14 +0200

    add OTSv2: don't ignore decision tree

---
+++ bnb/src/edu/aa12/OneTreeStrategyV2.java
@@ -0,0 +1,50 @@
+package edu.aa12;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Author: Philip Pickering <pgpick@gmx.at>
+ * Date: 5/30/12
+ * Time: 4:06 PM
+ */
+public class OneTreeStrategyV2 extends OneTreeStrategy {
+    @Override
+    public double lowerBound(Graph graph, BnBNode node) {
+        int one = 0;
+        while (isInDecTree(one, node) && one < graph.getVertices() - 1) {
+            one++;
+        }
+
+        BnBNode n = new BnBNode(null, null, false);
+        n = new BnBNode(n, node.edge, node.edgeIncluded);
+        List<Edge> edges = new LinkedList<Edge>(graph.incidentEdges[one]);
+
+        for (Edge e : edges) {
+            n = new BnBNode(n, e, false);
+        }
+
+
+        List<Edge> mst = kruskal.minimumSpanningTree(graph, n);
+
+        mst.add(popMin(edges));
+        mst.add(popMin(edges));
+
+        return cost(mst);
+    }
+
+    public boolean isInDecTree(int one, BnBNode node) {
+        BnBNode n = node;
+        while (n.parent != null) {
+            if (n.edge.u == one || n.edge.v == one) return true;
+            n = n.parent;
+        }
+
+        return false;
+
+    }
+
+    OneTreeStrategyV2(Graph g) {
+        super(g);
+    }
+}
