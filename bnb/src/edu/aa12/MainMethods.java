b3da05d39509107a51c7e97aa34c17309cdc192c
Thomas Jespersen <laumann.thomas@gmail.com>
2012-05-31 00:16:15 +0200

    Description of strategies

--- bnb/src/edu/aa12/MainMethods.java
+++ bnb/src/edu/aa12/MainMethods.java
@@ -2,25 +2,40 @@ package edu.aa12;
 
 /**
  * Here's a wicked-ass comment (or wicked ass-comment)
- * @author Thomas Jespersen <tj@asseco.dk>
  *
  */
 public class MainMethods {
 
 	public static void main(String[] args){
-        solveGraph(new Instance0());
+//        solveGraph(new Instance0());	// Our own little test instance
 		solveGraph(new Instance1());
 		solveGraph(new Instance2());
 //		solveGraph(new Instance3());
 	}
 	
 	public static void solveGraph(Graph g){
+		// Naive strategy
 		BranchAndBound_TSP solver = new BranchAndBound_TSP(g, new SimpleStrategy());
 		long start = System.nanoTime();
 		BnBNode n = solver.solve();
 		long end = System.nanoTime();
 		System.out.printf("Took %.2fms\n",(end-start)/1000000.0);
-//		System.out.println(n);
-//		Visualization.visualizeSolution(g, n);//Requires ProGAL (www.diku.dk/~rfonseca/ProGAL)
+
+		/*
+		 * Possible strategies to give the BnB solver:
+		 *  - SimpleStrategy
+		 *  - OneTreeStrategy
+		 *  - OneTreeStrategyVx, where x is a number in the range: { 2, 3, 4, 5, 6, 7 }
+		 *  - ZonesLowerBound
+		 *  - RelaxILPLowerBound  
+		 */
+		BranchAndBound_TSP zonesSolver = new BranchAndBound_TSP(g, new RelaxILPLowerBound());
+		long zstart = System.nanoTime();
+		BnBNode zonesNode = zonesSolver.solve();
+		long zend = System.nanoTime();
+		System.out.printf("Took %.2fms\n",(zend-zstart)/1000000.0);
+		
+//		System.out.println(zonesNode);
+		Visualization.visualizeSolution(g, n);//Requires ProGAL (www.diku.dk/~rfonseca/ProGAL)
 	}
 }
